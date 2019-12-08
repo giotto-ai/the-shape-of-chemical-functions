88e1c375a4cfa2a565524362bb58461f08b92c87
Francesco Pase <francesco.pase.work@gmail.com>
2019-12-08 17:23:42 +0100

    Working on code

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
-# shape-of-molecules
+giotto-learn
+============
+
+
+giotto-learn is a high performance topological machine learning toolbox in Python built on top of
+scikit-learn and is distributed under the Apache 2.0 license. It is part of the Giotto open-source project.
+
+Website: https://giotto.ai
+
+Installation
+------------
+
+Dependencies
+~~~~~~~~~~~~
+
+shape-of-molecules requires:
+
+- Python (>= 3.5)
+- scikit-learn (>= 0.21.3)
+- NumPy (>= 1.14.0)
+- joblib (>= 0.11)
+
+For running the examples jupyter, matplotlib and plotly are required.
+
+User installation
+~~~~~~~~~~~~~~~~~
+
+If you already have a working installation of numpy and scipy,
+the easiest way to install giotto-learn is using ``pip``   ::
+
+    pip install -U giotto-learn
+
+Documentation
+-------------
+
+- HTML documentation (stable release): https://docs.giotto.ai
+
+Contributing
+------------
+
+We welcome new contributors of all experience levels. The Giotto
+community goals are to be helpful, welcoming, and effective. To learn more about
+making a contribution to giotto-learn, please see the `CONTRIBUTING.rst
+<https://github.com/giotto-ai/giotto-learn/blob/master/CONTRIBUTING.rst>`_ file.
+
+Developer installation
+~~~~~~~~~~~~~~~~~~~~~~~
+
+C++ dependencies:
+'''''''''''''''''
+
+-  C++14 compatible compiler
+-  CMake >= 3.9
+-  Boost >= 1.56
+
+Source code
+'''''''''''
+
+You can check the latest sources with the command::
+
+    git clone https://github.com/giotto-ai/giotto-learn.git
+
+
+To install:
+'''''''''''
+
+.. code-block:: bash
+
+   cd giotto-learn
+   pip install -e .
+
+From there any change in the library files will be immediately available on your machine.
+
+Testing
+~~~~~~~
+
+After installation, you can launch the test suite from outside the
+source directory::
+
+    pytest giotto
+
+
+Changelog
+---------
+
+See the `RELEASE.rst <https://github.com/giotto-ai/giotto-learn/blob/master/RELEASE.rst>`__ file
+for a history of notable changes to giotto-learn.
+
+Important links
+~~~~~~~~~~~~~~~
+
+- Official source code repo: https://github.com/giotto-ai/giotto-learn
+- Download releases: https://pypi.org/project/giotto-learn/
+- Issue tracker: https://github.com/giotto-ai/giotto-learn/issues
+
+
+Contacts:
+---------
+
+maintainers@giotto.ai
--- molecules.py
+++ molecules.py
@@ -1,8 +1,9 @@
+import numpy as np
 import networkx as nx
-import pandas as pd
-from create_clique_complex import CreateCliqueComplex, CreateBoundaryMatrices, CreateLaplacianMatrices
-from heat_diffusion import HeatDiffusion
-from graph_entropy import GraphEntropy
+from giotto.graphs.create_clique_complex import CreateLaplacianMatrices, \
+    CreateCliqueComplex
+from giotto.graphs.heat_diffusion import HeatDiffusion
+from giotto.graphs.graph_entropy import GraphEntropy
 
 
 def mol_to_nx(mol):
@@ -24,14 +25,46 @@ def mol_to_nx(mol):
 
 def compute_node_edge_entropy(g, i, taus_n, taus_e):
     cd = CreateCliqueComplex(graph=g).create_complex_from_graph()
-    lap = CreateLaplacianMatrices().fit(cd, (0, 1)).transform(cd)
-    n_diff = HeatDiffusion().fit(lap[0], taus_n).transform(lap[0])
-    e_diff = HeatDiffusion().fit(lap[1], taus_e).transform(lap[1])
-    mh_n = GraphEntropy().fit(n_diff).transform(n_diff).T
-    mh_e = GraphEntropy().fit(e_diff).transform(e_diff).T
+    lap = CreateLaplacianMatrices().fit_transform(cd, (0, 1))
+    n_diff = HeatDiffusion().fit_transform(lap[0], taus_n)
+    e_diff = HeatDiffusion().fit_transform(lap[1], taus_e)
+    mh_n = GraphEntropy().fit_transform(n_diff).T
+    mh_e = GraphEntropy().fit_transform(e_diff).T
 
-    if i % 1000 == 0:
-        print(i)
+    if i % 10000 == 0:
+        print("Atoms and Bonds of {} molecules have been embedded...".
+              format(i))
+
     return [mh_n, mh_e]
 
 
+def bonds_type(g_mol):
+    for g in g_mol:
+        d_n = dict()
+        for n in g.nodes():
+            d_n[n] = np.zeros(4)
+
+            for i in g.neighbors(n):
+                # encoding type
+                edge_type = int(g.get_edge_data(n, i)['bond_type'])
+                if edge_type == 12:
+                    d_n[n][3] += 1
+                else:
+                    d_n[n][edge_type - 1] += 1
+        # Adding bonds information to each node of all molecules
+        nx.set_node_attributes(g, name='bonds_one_hot', values=d_n)
+
+
+def graph_to_points(g_mol, n):
+    j = 0
+    d = dict()
+    for i in range(len(g_mol)):
+        # if (g_mol[i].number_of_nodes() > 10 and g_mol[i].number_of_nodes() < 100):
+        if n==0:
+            d[i] = np.arange(j, j + g_mol[i].number_of_nodes())
+            j += g_mol[i].number_of_nodes()
+        else:
+            d[i] = np.arange(j, j + g_mol[i].number_of_edges())
+            j += g_mol[i].number_of_edges()
+
+    return d
